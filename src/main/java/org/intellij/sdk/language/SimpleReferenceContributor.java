ab9d7b9d3cf39c8854377a1b35061f856fdb7b9a
SaloEater <danil.lazarev@gmail.com>
2025-06-26 01:27:04 -0500

    contributor

--- src/main/java/org/intellij/sdk/language/SimpleReferenceContributor.java
+++ src/main/java/org/intellij/sdk/language/SimpleReferenceContributor.java
@@ -5,7 +5,10 @@ package org.intellij.sdk.language;
 import com.intellij.openapi.util.TextRange;
 import com.intellij.patterns.PlatformPatterns;
 import com.intellij.psi.*;
+import com.intellij.psi.impl.source.tree.LeafPsiElement;
 import com.intellij.util.ProcessingContext;
+import com.tang.intellij.lua.psi.*;
+import com.tang.intellij.lua.psi.impl.LuaLiteralExprImpl;
 import org.jetbrains.annotations.NotNull;
 
 import static org.intellij.sdk.language.SimpleAnnotator.SIMPLE_PREFIX_STR;
@@ -13,24 +16,41 @@ import static org.intellij.sdk.language.SimpleAnnotator.SIMPLE_SEPARATOR_STR;
 
 final class SimpleReferenceContributor extends PsiReferenceContributor {
 
-  @Override
-  public void registerReferenceProviders(@NotNull PsiReferenceRegistrar registrar) {
-    registrar.registerReferenceProvider(PlatformPatterns.psiElement(PsiLiteralExpression.class),
-        new PsiReferenceProvider() {
-          @Override
-          public PsiReference @NotNull [] getReferencesByElement(@NotNull PsiElement element,
-                                                                 @NotNull ProcessingContext context) {
-            PsiLiteralExpression literalExpression = (PsiLiteralExpression) element;
-            String value = literalExpression.getValue() instanceof String ?
-                (String) literalExpression.getValue() : null;
-            if ((value != null && value.startsWith(SIMPLE_PREFIX_STR + SIMPLE_SEPARATOR_STR))) {
-              TextRange property = new TextRange(SIMPLE_PREFIX_STR.length() + SIMPLE_SEPARATOR_STR.length() + 1,
-                  value.length() + 1);
-              return new PsiReference[]{new SimpleReference(element, property)};
-            }
-            return PsiReference.EMPTY_ARRAY;
-          }
-        });
-  }
+    @Override
+    public void registerReferenceProviders(@NotNull PsiReferenceRegistrar registrar) {
+        registrar.registerReferenceProvider(
+                PlatformPatterns
+                        .psiElement(LuaLiteralExpr.class)
+                        .withParent(LuaListArgs.class),
+                new PsiReferenceProvider() {
+                    @Override
+                    public PsiReference @NotNull [] getReferencesByElement(@NotNull PsiElement element,
+                                                                           @NotNull ProcessingContext context) {
+                        LuaLiteralExprImpl literalExpression = (LuaLiteralExprImpl) element;
+                        var child = (LeafPsiElement) literalExpression.getFirstChild();
+                        if (child == null || child.getElementType() != LuaTypes.STRING) {
+                            return PsiReference.EMPTY_ARRAY;
+                        }
+                        String value = child.getText().substring(1, child.getTextLength() - 1);
+                        if ((value != null && value.startsWith(SIMPLE_PREFIX_STR + SIMPLE_SEPARATOR_STR))) {
+                            TextRange property = new TextRange(SIMPLE_PREFIX_STR.length() + SIMPLE_SEPARATOR_STR.length() + 1,
+                                    value.length() + 1);
+                            return new PsiReference[]{new SimpleReference(element, property)};
+                        }
+                        return PsiReference.EMPTY_ARRAY;
+                    }
+                });
+
+        registrar.registerReferenceProvider(
+                PlatformPatterns
+                        .psiElement(PsiElement.class),
+                new PsiReferenceProvider() {
+                    @Override
+                    public PsiReference @NotNull [] getReferencesByElement(@NotNull PsiElement element,
+                                                                           @NotNull ProcessingContext context) {
+                        return PsiReference.EMPTY_ARRAY;
+                    }
+                });
+    }
 
 }
